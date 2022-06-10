88f21db8f9651de32012274f9f81ec15cf9d85ed
jeremylfchen <jeremylfchen@gmail.com>
2022-06-09 18:50:37 -0700

    CSS WIP

--- ui/src/components/SortByFrequency.vue
+++ ui/src/components/SortByFrequency.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <div>
-      <input v-model="input">
+  <div class="container">
+    <div class="col" id="input">
+      <textarea v-model="input" rows="4" cols="50" />
       <button @click="onClick">Submit</button>
     </div>
-    <div>
+    <div class="col" id="display">
       <ul v-for="tuple in output" :key="tuple[0]" style="list-style-type:none;">
         <li>
           {{`${tuple[0]} ${tuple[1]}`}}
@@ -32,6 +32,7 @@ export default {
           const response = await axios.post('/sortByFrequency', {
             input: this.input
           });
+          this.input = '';
           this.output = response.data;
         } catch (e) {
           console.error(e);
@@ -41,3 +42,21 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    justify-content: center;
+    align-content: center;
+  }
+  #display {
+    min-height: 100px;
+    max-height: 500px;
+    max-width: 400px;
+    overflow: auto;
+  }
+  li {
+    text-align: left;
+  }
+</style>
