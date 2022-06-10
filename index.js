9b92030b0dcebbd5d2a8ad18efe9353f0ade167f
jeremylfchen <jeremylfchen@gmail.com>
2022-06-09 18:08:20 -0700

    Rename folder and setup post API

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const sortByFrequency = require('./functions/sortByFrequency');
 
+app.use(express.json());
 app.use('/', express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.post('/sortByFrequency', (req, res) => {
+  let { input } = req.body;
+  const result = sortByFrequency(input);
+  res.send(result);
 });
 
 app.listen(port, () => {
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: '../public'
+  outputDir: '../public',
+  devServer: {
+    proxy: 'http://localhost:3000',
+  }
 })
